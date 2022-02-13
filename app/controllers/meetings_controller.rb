d3ff3256ab594eb435bc5930cf36919d0b1bddcf
Alex Krueger <alexckrueger@gmail.com>
2022-02-13 13:06:18 -0600

    adds more routes and enables cors

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -9,4 +9,40 @@ class MeetingsController < ApplicationController
     render json: holidays
   end
 
+  def create
+    meeting = Meeting.new(
+      title: params[:title],
+      location: params[:location],
+      date: params[:date]
+    )
+    if meeting.save
+      render json: meeting
+    else
+      render json: {errors: meeting.errors.full_messages}, status: 422
+    end
+  end
+
+  def show
+    meeting = Meeting.find(params[:id])
+    render json: meeting
+  end
+
+  def update
+    meeting = Meeting.find(params[:id])
+    meeting.title = params[:title] || meeting.title
+    meeting.location = params[:location] || meeting.location
+    meeting.date = params[:date] || meeting.date
+    if meeting.save
+      render json: meeting
+    else
+      render json: {errors: meeting.errors.full_messages}, status: 422
+    end
+  end
+
+  def destroy
+    meeting = Meeting.find(params[:id])
+    meeting.destroy
+    render json: {message: "Meeting successfully obliterated."}
+  end
+
 end
